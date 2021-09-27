0076043f45865ca95a8549e647a349b70bc67d25
Azor Ahai <89867876+AzorAhaiMkt@users.noreply.github.com>
2021-09-27 14:17:21 -0300

    Upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function github() {
+  window.open("https://github.com/AzorAhaiMkt");
+}
+
+function amino() {
+  window.open("http://aminoapps.com/p/a2u41n");
+}
+
+function temaEscuro() {
+  document.body.classList.toggle("dark")
+}
